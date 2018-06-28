cef947baca203f90cea227300e11530cbb90a6a6
Federico Cergol <federico.cergol@gmail.com>
2018-06-28 20:54:24 +0200

    dev: added address logging

--- src/beat.c
+++ src/beat.c
@@ -1,5 +1,6 @@
 #include <arpa/inet.h>
 #include <errno.h>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <signal.h>
 #include <stdbool.h>
@@ -17,10 +18,11 @@ int port = 0xBEA7;
 
 int tcp_server_socket;
 int tcp_sock;
+pid_t fork_pid;
 bool stopping = false;
 
 void removepidfile() {
-    if (unlink(pidfilename) < 0) {
+    if (fork_pid && unlink(pidfilename) < 0) {
         fprintf(stderr, "Unable to remove pidfile, error: %d", errno);
     }
 }
@@ -35,34 +37,37 @@ void die(int code, char *message, ...) {
 void signal_handler(int signo) {
     if (signo == SIGINT || signo == SIGTERM) {
         printf("Received stop signal\n");
-        stopping = true;
+        if (tcp_server_socket != 0)
+            close(tcp_server_socket);
+        if (tcp_sock != 0)
+            close(tcp_sock);
+        removepidfile();
+        exit(0);
     }
 }
 
 int main(int argc, char **argv) {
 
-    pid_t fork_pid = fork();
+    fork_pid = argc < 2 ? fork() : 0;
 
     if (fork_pid == 0) {
         // child
 
-        FILE *pid_file = fopen(pidfilename, "w");
-        if (pid_file == NULL) {
-            die(errno, "Error creating file: %d\n", errno);
-        }
-        if (fprintf(pid_file, "%d", getpid()) < 0) {
-            die(-1, "Error trying to write pid to file\n");
-        }
-        if (fclose(pid_file) != 0) {
-            die(errno, "Error closing pidfile: %d\n", errno);
+        if (argc < 2) {
+            FILE *pid_file = fopen(pidfilename, "w");
+            if (pid_file == NULL) {
+                die(errno, "Error creating file: %d\n", errno);
+            }
+            if (fprintf(pid_file, "%d", getpid()) < 0) {
+                die(-1, "Error trying to write pid to file\n");
+            }
+            if (fclose(pid_file) != 0) {
+                die(errno, "Error closing pidfile: %d\n", errno);
+            }
         }
 
-        struct sigaction action;
-        action.sa_handler = signal_handler;
-        action.sa_flags = 0;
-        sigemptyset(&action.sa_mask);
-        sigaction(SIGINT, &action, NULL);
-        sigaction(SIGTERM, &action, NULL);
+        signal(SIGINT, signal_handler);
+        signal(SIGTERM, signal_handler);
 
         struct sockaddr_in server_address;
         memset(&server_address, 0, sizeof(server_address));
@@ -84,13 +89,17 @@ int main(int argc, char **argv) {
         }
 
         while (1) {
-            tcp_sock = accept(tcp_server_socket, NULL, NULL);
-            if (stopping) {
-                printf("Stopping\n");
-                close(tcp_sock);
-                close(tcp_server_socket);
-                removepidfile();
-                exit(0);
+            struct sockaddr_in client_addr;
+            socklen_t sockaddr_len;
+            tcp_sock = accept(tcp_server_socket, (struct sockaddr *)&client_addr, &sockaddr_len);
+            char out[64];
+            int err = getnameinfo((struct sockaddr *)&client_addr, sockaddr_len, out, 64, NULL, 0, NI_NUMERICHOST);
+            if (err != 0) {
+                fprintf(stderr, "Error decoding: %d %d %d\n", err, errno, sockaddr_len);
+                fprintf(stderr, "Error: %s\n", gai_strerror(err));
+            } else {
+                printf("Client connected: %s\n", out);
+                fflush(stdout);
             }
 
             if (tcp_sock < 0) {
@@ -99,10 +108,12 @@ int main(int argc, char **argv) {
 
             if (send(tcp_sock, message, strlen(message), 0) < 0) {
                 fprintf(stderr, "Cannot send message to socket, error: %d\n", errno);
+                close(tcp_sock);
             }
 
             if (close(tcp_sock) < 0) {
                 fprintf(stderr, "Cannot close socket, error: %d\n", errno);
+                close(tcp_sock);
             }
         }
     } else {
