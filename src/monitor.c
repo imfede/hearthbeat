2b85f8bde55e64396e1713cca4a3a0cb206db6d6
imfede <federico.cergol@gmail.com>
2018-07-05 21:19:22 +0200

    dev: simple working monitor

--- src/monitor.c
+++ src/monitor.c
@@ -11,6 +11,9 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <event.h>
+#include <sys/time.h>
+#include <pthread.h>
 
 int udp_server_socket = 0;
 struct sockaddr_in target_addr = {0};
@@ -47,47 +50,17 @@ void send_bip() {
     }
 }
 
-void handle_signal(int sig) {
-    switch (sig) {
-    case SIGALRM:
-        send_bip();
-        alarm(interval);
-        break;
-    default:
-        fprintf(stderr, "Not implemented!\n");
-        break;
-    }
+void handle_event(int fd, short event, void *arg) {
+    send_bip();
 }
 
-void set_timer() {
-    signal(SIGALRM, &handle_signal);
-    alarm(interval);
-}
-
-int main() {
-    init();
-    struct sockaddr_in server_address;
-    memset(&server_address, 0, sizeof(server_address));
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    // create and bind UDP
-    udp_server_socket = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (udp_server_socket < 0) {
-        die(-1, "Error creating UDP socket\n");
-    }
-
-    if (bind(udp_server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
-        die(errno, "Cannot bind UDP socket, error: %d\n", errno);
-    }
-
+void *handle_connection_thread(void *ign) {
     nfds_t nfds = 1;
     struct pollfd fds[nfds];
     memset(fds, 0, sizeof(fds));
     fds[0].fd = udp_server_socket;
     fds[0].events = POLLIN;
 
-    set_timer();
     while (true) {
         int ret = poll(fds, nfds, -1);
 
@@ -104,4 +77,41 @@ int main() {
             }
         }
     }
+}
+
+int main() {
+    init();
+
+    struct event_base *base = event_base_new();
+    struct event *ev;
+    struct timeval tv;
+
+    tv.tv_sec = 1;
+    tv.tv_usec = 0;
+
+    ev = event_new(base, -1, EV_PERSIST, &handle_event, NULL);
+    evtimer_add(ev, &tv);
+
+    struct sockaddr_in server_address;
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    // create and bind UDP
+    udp_server_socket = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (udp_server_socket < 0) {
+        die(-1, "Error creating UDP socket\n");
+    }
+
+    if (bind(udp_server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
+        die(errno, "Cannot bind UDP socket, error: %d\n", errno);
+    }
+
+    pthread_t listener;
+    int thread_status = pthread_create(&listener, NULL, &handle_connection_thread, NULL);
+    if(thread_status != 0) {
+        die(thread_status, "Error threading: %d\n", thread_status);
+    }
+
+    event_base_dispatch(base);
 }
