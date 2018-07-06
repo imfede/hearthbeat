bc8e79fe51f410eb5f83692934c4a93c276a490c
imfede <federico.cergol@gmail.com>
2018-07-06 23:25:04 +0200

    enh: argparse tests

--- test/argparse.c
+++ test/argparse.c
@@ -11,14 +11,35 @@ int test_list() {
 
     struct list_cell *it = list;
 
+    int i = 0;
     while(it != NULL) {
-        printf("%s\n", it->value);
+        printf("list element %d: %s\n", i, it->value);
         it = it->next;
+        i += 1;
     }
 
     free_list_cell(list);
 }
 
+int test_str() {
+    char *str;
+    argparse_register_argument_str("str", &str);
+    argparse_read_properties("test/test_list.txt");
+
+    printf("str: %s\n", str);
+    free(str);
+}
+
+int test_int() {
+    int i;
+    argparse_register_argument_int("int", &i);
+    argparse_read_properties("test/test_list.txt");
+
+    printf("int: %d\n", i);
+}
+
 int main() {
     test_list();
+    test_str();
+    test_int();
 }
